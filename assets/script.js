83e0be6c35c9c8a400c13349ec0b08ccd3a7f858
Michael Fowler <redfaction8008@gmail.com>
2023-05-11 10:36:23 -0500

    modified the html classes added a new script to pull the data from the api added a new script add local storage

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,78 @@
 
+  //apiKey: "3d10b270100701ab93bcc7db3d85dd79"
+  document.addEventListener('DOMContentLoaded', function () {
+    var searchButton = document.querySelector('.btn-primary');
+  
+    searchButton.addEventListener('click', function () {
+      var cityInput = document.querySelector('.form-control').value;
+      fetchWeatherData(cityInput);
+    });
+  
+    function fetchWeatherData(city) {
+      var apiKey = "3d10b270100701ab93bcc7db3d85dd79";
+      var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  
+      fetch(apiUrl)
+        .then(function (response) {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Error: ' + response.status);
+          }
+        })
+        .then(function (data) {
+          var temperature = data.main.temp;
+          var windSpeed = data.wind.speed;
+          var humidity = data.main.humidity;
+  
+          var tempElement = document.querySelector('.temp');
+          tempElement.textContent = temperature + '°F';
+  
+          var cityElement = document.querySelector('.city');
+          cityElement.textContent = data.name;
+  
+          var windElement = document.getElementById('wind');
+          windElement.textContent = 'Wind: ' + windSpeed + ' m/s';
+  
+          var humidityElement = document.getElementById('Humidity');
+          humidityElement.textContent = 'Humidity: ' + humidity + '%';
 
-const apiKey = "3d10b270100701ab93bcc7db3d85dd79";
-const url = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
 
-const searchBox = document.getElementById("#search input");
-const searchBtn = document.getElementById("#search button");
+    
+          logRecentSearch(data.name);
+        })
+        .catch(function (error) {
+          console.log('Error:', error);
+        });
+    }
+  
+  
+  
+    function logRecentSearch(city) {
+      var recentSearches = JSON.parse(localStorage.getItem('recentSearches')) || [];
+      recentSearches.unshift(city);
+  
+      if (recentSearches.length > 5) {
+        recentSearches = recentSearches.slice(0, 5);
+      }
+  
+      localStorage.setItem('recentSearches', JSON.stringify(recentSearches));
+  
+      loadRecentSearches();
+    }
+  
+    function loadRecentSearches() {
+      var recentSearches = JSON.parse(localStorage.getItem('recentSearches')) || [];
+      var recentSearchesElement = document.querySelector('.Reascent .card-text');
+      recentSearchesElement.innerHTML = '';
+  
+      recentSearches.forEach(function (search) {
+        var searchItem = document.createElement('p');
+        searchItem.textContent = search;
+        recentSearchesElement.appendChild(searchItem);
+      });
+    }
 
-async function getWeather() {
-  const response = await fetch(url + city + "&appid=" + apiKey);
-  const data = await response.json();
-  console.log(data);
-}
-
-
-
-document.getElementById("#city").innerHTML = data.name;
-document.getElementById("#temp").innerHTML = data.main.temp + "°F";
-document.getElementById("#humidity").innerHTML = data.main.humidity + "%";
-document.getElementById("#wind").innerHTML = data.wind.speed + "mph";
-
-searchBtn.addEventListener("click", () => {
-  getWeather(searchBox.value);
-});
+  
+    loadRecentSearches();
+  });
